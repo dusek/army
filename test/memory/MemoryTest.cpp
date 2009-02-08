b7a1a58419b45530cd292323cfc11af7ab6e8a36
Boris Dusek <boris.dusek@gmail.com>
2009-02-08 20:31:33 +0100

    Optimize the tests a little bit.

--- test/memory/MemoryTest.cpp
+++ test/memory/MemoryTest.cpp
@@ -35,15 +35,17 @@ void MemoryTest::testBasic()
 
     //Read the just written memory in various ways and compare with expected parts
     //of "written" string
+    std::string slice = written.substr(10, 5);
     for (i = 0; i < Count; i++) {
         addr = i*(written.length() + gap) + init_offset;
         read = mem->read(addr + 10, 5);
-        CPPUNIT_ASSERT_EQUAL(written.substr(10, 5), read);
+        CPPUNIT_ASSERT_EQUAL(slice, read);
         read = mem->read(addr, written.length());
         CPPUNIT_ASSERT_EQUAL(written, read);
     }
 
     // Read of size 0 always returns empty string
+    std::string empty("");
     for (addr = 0; addr < init_offset + (Count - 1)*(written.length() + gap) + written.length(); addr++)
-        CPPUNIT_ASSERT_EQUAL(std::string(""), mem->read(addr, 0));
+        CPPUNIT_ASSERT_EQUAL(empty, mem->read(addr, 0));
 }
